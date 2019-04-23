f12d807498b1ba88d983ee77c6e8538f111a4549
Michael Kelley <mx@kell4.com>
2019-04-23 11:05:29 -0400

    Initial Commit

---
+++ AllerCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllerCheck
+//
+//  Created by Michael X Kelley on 4/23/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
